66de3ccb43e948f0a116d1263c2ab29706d402de
ElenaCherpakova <elena.cherpakova@gmail.com>
2025-06-15 19:36:55 +0800

    clean data by normalizing names remove spacing and update team names

--- data_wrangling.py
+++ data_wrangling.py
@@ -1,6 +1,36 @@
 import pandas as pd
-
+import unicodedata
 raw_players = pd.read_csv('csv/players.csv')
+
+duplicates = raw_players.groupby('Player_ID')['Name'].nunique()
+conflicts = duplicates[duplicates > 1]
+
+if not conflicts.empty:
+    print('Found conflicts of Player_ID:')
+    conflict_ids = conflicts.index.to_list()
+    print(raw_players[raw_players['Player_ID'].isin(
+        conflict_ids)][['Player_ID', 'Name']].drop_duplicates())
+
+correct_players_name = {
+    "Brett Gardner":"gardnbr01",
+    "Josh Donaldson": "donaljo02"
+}
+
+# Clean names by adding spaces if there is none and remove any normalized characters if there are diacritics (é)
+def clean_name(name):
+    name = name.strip()
+    chars = []
+    normalized_name = unicodedata.normalize('NFD', name)
+    for char in normalized_name: 
+        if unicodedata.category(char) != "Mn":
+            chars.append(char)
+    name = ''.join(chars)    
+    res = name[0]
+    for char in name[1:]:
+        if char.isupper() and res[-1] != ' ':
+            res += ' '
+        res += char
+    return ' '.join(res.split()).title()
 
 team_name_mapping = {
     "Oakland": "Oakland Athletics",
@@ -21,6 +51,18 @@ team_name_mapping = {
     "Cleveland": "Cleveland Indians"
 }
 
+
+raw_players['Name'] = raw_players['Name'].apply(clean_name)
+
+
+for name, player_id in correct_players_name.items():
+    raw_players.loc[raw_players['Name'] == name, 'Player_ID'] = player_id
+
 raw_players['Team'] = raw_players['Team'].map(
     team_name_mapping).fillna(raw_players['Team'])
+
+# print(raw_players.loc[raw_players['Name'] == "Brett Gardner", ['Player_ID', 'Name']])
+# print(raw_players.loc[raw_players['Name'] == "Josh Donaldson", ['Player_ID', 'Name']])
+
 raw_players.to_csv('csv/cleaned_players.csv', index=False)
+
--- streamlit.py
+++ streamlit.py
@@ -1,19 +1,35 @@
 import streamlit as st
 import pandas as pd
 
-df_year = pd.read_csv('csv/players.csv')
+players_fd = pd.read_csv('csv/cleaned_players.csv')
+player_ids = players_fd[['Player_ID', 'Name']].drop_duplicates()
+player_ids_sorted = player_ids.sort_values('Player_ID')
+player_dict = player_ids_sorted.set_index('Player_ID')['Name'].to_dict()
+print(player_dict)
 
 st.title("""⚾️  Explore The History of the American Baseball League From 2000 to 2024""")
 st.sidebar.title('Filter:')
 
-year_selected = st.sidebar.slider("⏳ Select year:",
-                                  min_value=int(df_year['Year'].min()),
-                                  max_value=int(df_year['Year'].max()),
+year_selected = st.sidebar.slider("⏳ Select a year:",
+                                  min_value=int(players_fd['Year'].min()),
+                                  max_value=int(players_fd['Year'].max()),
                                   value=2012,
                                   step=1)
 
-player_name = st.sidebar.selectbox("🧢 **Choose a player:** ",
-                                   sorted(df_year['Name'].unique())
-                                   )
 
-year_df = df_year['Year'] == year_selected
+
+select_player_id = st.sidebar.selectbox("🧢 **Choose a player:** ",
+                                        options=player_dict.keys(),
+                                        format_func=lambda pid: player_dict[pid])
+
+selected_columns = ['Name', 'Team', 'Statistic', 'Value', 'Year']
+
+player_data = players_fd[players_fd['Player_ID'] == select_player_id]
+player_df_display = player_data[selected_columns]
+year_df = players_fd[players_fd['Year'] == year_selected]
+st.subheader(f"Year in review: **{year_selected}** American League")
+st.dataframe(year_df, hide_index=True)
+
+player_name = player_dict.get(select_player_id)
+st.subheader(f"Baseball Stats for **{player_name}**")
+st.dataframe(player_df_display, hide_index=True)
