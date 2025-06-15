60829d3be0b29ed1cc218abc7b2c911fc80d858e
ElenaCherpakova <elena.cherpakova@gmail.com>
2025-06-16 00:39:16 +0800

    add streamlit for career length

--- streamlit.py
+++ streamlit.py
@@ -1,16 +1,17 @@
 import streamlit as st
 import pandas as pd
+import altair as alt
 
 players_fd = pd.read_csv('csv/cleaned_players.csv')
+career_fd = pd.read_csv("csv/players_stats.csv")
+players_fd["Year"] = players_fd["Year"].astype(int)
+career_fd["Career_Length"] = career_fd["Career_Length"].astype(int)
 player_ids = players_fd[['Player_ID', 'Name']].drop_duplicates()
-team_ids = players_fd[['Player_ID', 'Team']].drop_duplicates()
-player_ids_sorted = player_ids.sort_values('Player_ID')
-player_dict = player_ids_sorted.set_index('Player_ID')['Name'].to_dict()
+career_fd = career_fd.merge(player_ids, on="Player_ID", how="left")
+
+player_dict = player_ids.set_index('Player_ID')['Name'].to_dict()
 
 teams = sorted(players_fd['Team'].dropna().unique())
-
-
-print(teams)
 
 st.title("""⚾️  Explore The History of the American Baseball League From 2000 to 2024""")
 st.sidebar.title('Filter:')
@@ -30,15 +31,38 @@ select_player_id = st.sidebar.selectbox("🧢 **Choose a player:** ",
 team_selected = st.sidebar.selectbox("⚾️⚾️ **Choose a team:**",
                                         options=teams)
 selected_columns = ['Name', 'Team', 'Statistic', 'Value', 'Year']
-selected_columns_for_team = ['Name', 'Team', 'Year']
+selected_columns_for_team = ['Name', 'Year']
 player_df = players_fd[players_fd['Player_ID'] == select_player_id][selected_columns]
 year_df = players_fd[players_fd['Year'] == year_selected][selected_columns]
-team_df = players_fd[players_fd['Team'] == team_selected][selected_columns_for_team].drop_duplicates().sort_values(by=['Name', 'Year'])
+team_df = players_fd[players_fd['Team'] == team_selected][selected_columns_for_team]
+team_data_df = team_df.drop_duplicates().sort_values(by=['Name', 'Year'])
 st.subheader(f"Year in review: **{year_selected}** American League", divider=True)
 st.dataframe(year_df, hide_index=True)
 
-player_name = player_dict.get(select_player_id)
-st.subheader(f"Baseball Stats for **{player_name}**", divider=True)
+st.subheader(f"Baseball Stats for **{player_dict[select_player_id]}**", divider=True)
 st.dataframe(player_df, hide_index=True)
 st.subheader(f"Team in review: **{team_selected}**", divider=True)
-st.dataframe(team_df, hide_index=True)
+st.dataframe(team_data_df, hide_index=True)
+
+avg_career_length = career_fd['Career_Length'].mean()
+col1, col2= st.columns(2)
+with col1:
+    st.subheader("Distribution of Career Lengths")
+with col2:
+    st.metric("📊 Average Career Length", f"{avg_career_length:.1f} years")
+   
+   
+col1, col2 = st.columns(2, gap='large') 
+
+with col1:
+    chart = alt.Chart(career_fd).mark_bar().encode(
+        alt.X("Career_Length:Q", bin=alt.Bin(step=1), title="Career Length (years)",  axis=alt.Axis(format="d")),
+        y='count()',
+    )
+    st.altair_chart(chart)
+with col2:
+    top_3_career = career_fd.sort_values('Career_Length', ascending=False).head(3)
+    st.subheader("🏆 Top 3 Players with the Longest Careers")
+    st.dataframe(top_3_career[['Name', 'Career_Length']], hide_index=True)
+
+
